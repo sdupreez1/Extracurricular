33503713b9e8a538a6eb3350dab0cb0aecee8a5e
Seth du Preez <sethdupreez@gmail.com>
2024-09-02 19:31:51 +0100

    Rosalind Bioinf Armory Questions

--- Bioinformatics/Rosalind/rosalind_python/Bioinformatics Armory.py
+++ Bioinformatics/Rosalind/rosalind_python/Bioinformatics Armory.py
@@ -1,5 +1,6 @@
-from Bio.Seq import Seq
-from Bio import Entrez, SeqIO #can't import translate?
+from Bio.Seq import Seq, translate
+from Bio import Entrez, SeqIO 
+from Bio.Data import CodonTable
 import re
 
 # 1 - Introduction to the Bioinformatics Armory
@@ -35,7 +36,7 @@ with open('data/rosalind_frmt.txt') as idfile3:
     
     fastamin3 = '>{}\n{}'.format(minseq3.description, minseq3.seq)
 
-# 5 - Pairwise Global Alignment (Emboss Scoring Matricies)
+# 5 - Pairwise Global Alignment (Emboss Scoring Matricies) [Online - Emboss]
 with open('data/rosalind_need.txt') as idfile5:
     ids5 = idfile5.read().split()
     handle5 = Entrez.efetch(
@@ -74,29 +75,49 @@ def mean(ls):
 phds_avg7 = [mean(phd) for phd in phds7]
 phds_low7 = [phd for phd in phds_avg7 if phd<float(phd_thresh7)]
 
-# 8 - Protein Translation
-with open('data/codon_to_aa.txt', 'r') as transl_table:
-    rna_to_aa = transl_table.read().split()
-    rna_to_aa = {cdn:aa for cdn, aa in zip(rna_to_aa[::2], rna_to_aa[1::2])}
-
-dna_bases = ['A', 'C', 'G', 'T']
-dna_to_rna = {'A':'u', 'C':'g', 'G':'c', 'T':'a'}
-
+# 8 - Protein Translation (Using Different Translation Tables)
 with open('data/rosalind_ptra.txt') as str_prt:    
     s8 = str_prt.read().split('\n')
     prt8 = s8[1]
     s8 = s8[0]
 
-def transcribe(s):
-    for base in dna_bases:
-        s = s.replace(base, dna_to_rna[base])
-    s = s.upper()
-    return s
+table_ids = [id_num for id_num in sorted(CodonTable.generic_by_id)]
 
-def translate(s):
-    return ''.join([
-        rna_to_aa[codon] for codon in  list(map(''.join, zip(*[iter(s)]*3)))
-        ])
+for table_id in table_ids: # There are currently 33 tables as of 02/09/2024
+    match_point = [(match.start() for match 
+                       in list(re.finditer(prt8, translate(s8, table_id))))]
+    if bool(match_point) == True:
+        print(table_id, match_point, translate(s8, table_id))
 
-transl8 = translate(transcribe(s8))
-[match.start() for match in list(re.finditer(prt8, transl8))]
+# 9 - Read Filtration by Quality (Filter Better Quality Reads) 
+#     [Online - Galaxy for FastQC]
+with (open('data/rosalind_filt.txt') as infile9,
+      open('data/rosalind_fastqual.txt', 'w') as outfile9):
+    # crit9 = re.findall('^.*(?=\n)', all9)[0].split()  # Look for better method
+    crit9 = infile9.readline().strip().split()  
+        # readline() 'consumes' the first line
+    outfile9.write(infile9.read()) 
+        # read() 'consumes' the remaining text (without the first line since 
+        # it's been used in readline() above)
+
+# 10 - Complementing a Strand of DNA
+with open('data/rosalind_rvco.txt') as infile10:
+    seq10 = list(SeqIO.parse(infile10, 'fasta'))
+    seq10 = [s.seq for s in seq10]
+    rev_comp10 = [s.reverse_complement() for s in seq10]
+    rev_same10 = [s for s in seq10 if rev_comp10[seq10.index(s)] == s]
+    len(rev_same10)
+    
+# 12 - Base Quality Distrtibution
+with open('data/rosalind_bphr.txt') as infile12:
+    crit12, fastq12 = infile12.readline(), infile12.read()
+    open('data/test_fastq.txt', 'w').write(fastq12)
+    
+    records12 = list(SeqIO.parse('data/test_fastq.txt', 'fastq'))
+    phd_list12 = [s.letter_annotations['phred_quality'] for s in records12]
+    phd_bybase12 = [base for phds in phd_list12 for base in phds]
+    phd_avg12 = [
+        mean([p[i] for p in phd_list12]) for i in range(len(phd_list12[0]))
+        ]
+    invalid_phds12 = [base for base in phd_avg12 if base < int(crit12)]
+    len(invalid_phds12)
