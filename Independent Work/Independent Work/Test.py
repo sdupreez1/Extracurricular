2bfd33ca68aeab06edd9bcde9a736c5f165d85d7
Seth du Preez <sethdupreez@gmail.com>
2024-10-11 18:45:02 +0100

    Moving Files

---
+++ Independent Work/Independent Work/Test.py
@@ -0,0 +1,20 @@
+from Bio.Seq import Seq
+from Bio import Entrez, SeqIO
+import re
+
+print('ehllo word')
+
+# Find similarites and differences between RNA and DNA Polymerase
+
+# Variance between structure/expression of same(?) proteins in different species 
+
+# UKBB
+
+# Find transposon sequence (not just the number of occurances)
+
+# Model/detect antibiotic resistance evolution in bacteria
+
+# Impact of heterozygousity(?) proportion on expression of mutation 
+# (eg in mtDNA)
+
+# 
